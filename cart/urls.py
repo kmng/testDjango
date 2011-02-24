da752c085bf09bd786a939e5701e04f6689ab211
kmng <kmng@ucla.edu>
2011-02-23 16:53:59 -0800

    cart model

---
+++ cart/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('project1.cart.views',
+            (r'^$','show_cart',{'template_name':'cart/cart.html'},'show_cart'),
+            )
+
+                    
