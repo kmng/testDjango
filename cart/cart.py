88e4a37f2be4e1456123ac7df9dcbd725a29b256
kmng <kmng@ucla.edu>
2011-02-25 23:46:14 -0800

    cart update 2

--- cart/cart.py
+++ cart/cart.py
@@ -4,3 +4,51 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 import decimal
 import random
+
+# get the current user's cart ID, set new one if blank
+def _cart_id(request):
+    if request.session.get(CART_ID_SESSION_KEY,'') == '':
+        request.session[CART_ID_SESSION_KEY] = _generate_cart_id()
+    return request.session[CART_ID_SESSION_KEY]
+
+def _generate_cart_id():
+    cart_id = ''
+    characters = 'ABCDEFGHIJLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+            1234567890!@#$%^&*()'
+    cart_id_length = 50
+    for y in range(cart_id_length):
+        card_id += characters[random.randint(0,len(characters) - 1)]
+    return card_id
+
+# return all items from the current user's cart
+def get_cart_items(request):
+    return CartItem.objects.filter(cart_id = _card_id(request))
+
+#add an item to the cart
+def add_to_cart(request):
+    postdata = request.POST.copy()
+    product_slug = postdata.get('product_slug','')
+    quantity = postdata.get('quantity',1)
+    p = get_object_or_404(Product,slug=product_slug)
+    cart_products = get_cart_item(request)
+    for cart_item in cart_products:
+        if cart_item.product.id = p.id:
+            cart_item.augment_quantity(quantity)
+            product_in_cart = True
+
+    if not product_int_cart:
+        ci = CartItem()
+        ci.product = p
+        ci.quantity = quantity
+        ci.cart_id = _cart_id(request)
+        ci.save()
+
+def cart_distinct_item_count(request):
+    return get_cart_items(request).count()
+
+        
+
+
+
+    
+    
--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from project1.cart import cart
 
-def show_cart(request, template_name="cart/cart.html")
+def show_cart(request, template_name="cart/cart.html"):
     cart_item_count = cart.cart_item_count(request)
     page_title = 'Shopping Cart'
     return render_to_response(template_name,locals(),
