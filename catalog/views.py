e5db1e90dd582494db694913fba1a7bcdbcfe9c1
kmng <kmng@ucla.edu>
2011-02-25 23:56:50 -0800

    add form view

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,11 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from project1.catalog.models import  Category, Product
 from django.template import RequestContext
+from django.core import urlresolvers
+from project1.cart import cart
+from django.http import HttpResponseRedirect
+from project1.cart.forms import ProductAddToCartForm
+
 
 def index(request,template_name = 'catalog/index.html'):
     page_title = 'Musical Instuments and Sheet Music for Musicians'
@@ -23,6 +28,12 @@ def show_product(request,product_slug,template_name='catalog/product.html'):
     page_title = p.name
     meta_keywords = p.meta_keywords
     meta_description = p.meta_description
+    if request.method == 'POST':
+        postdata = request.POST.copy()
+        from = ProductAddToCartForm(postdata)
+        if form.is_valid():
+            cart.add_to_cart(request)
+            if
     return render_to_response(template_name,locals(),
             context_instance = RequestContext(request))
      
