752e0b46e1f4afb42f048acb243dcf3405ee61f3
kmng <kmng@ucla.edu>
2011-02-25 22:20:38 -0800

    cart part I

---
+++ cart/cart.py
@@ -0,0 +1,6 @@
+from project1.cart.models import CartItem
+from project1.catalog.models import Product
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+import decimal
+import random
