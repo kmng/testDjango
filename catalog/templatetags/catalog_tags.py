4fd486e343d1bd0bce596c09cd17bde058c33b3b
kmng <kmng@ucla.edu>
2011-03-25 20:24:40 -0700

    update tag

--- catalog/templatetags/catalog_tags.py
+++ catalog/templatetags/catalog_tags.py
@@ -1,8 +1,19 @@
 from django import template
 from project1.cart import cart
+from project1.catalog.models import Category
+
 
 register = template.Library()
 @register.inclusion_tag("tags/cart_box.html")
 def cart_box(request):
     cart_item_count = cart.cart_distinct_item_count(request)
     return {'cart_item_count':cart_item_count}
+
+@register.inclusion_tag("tags/category_list.html")
+def category_list(request_path):
+    active_categories = Category.objects.filter(is_active=True)
+    return {
+            'active_categories':active_categories,
+            'request_path':request_path
+        }
+    
